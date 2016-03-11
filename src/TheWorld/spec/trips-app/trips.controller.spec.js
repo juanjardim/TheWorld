3b8c1d7711de514411fc494b8997be703c421f76
juan jardim <juanjardim@gmail.com>
2016-03-11 21:33:17 +0100

    Create TripsController to get all User trips. Also create a data service to manage api request.

---
+++ src/TheWorld/spec/trips-app/trips.controller.spec.js
@@ -0,0 +1,21 @@
+﻿describe("Trips Controller", function() {
+    var $vm, $controller;
+
+    beforeEach(module('app-trips'));
+
+    beforeEach(inject(function(_$controller_) {
+        $controller = _$controller_
+    }));
+
+    it("Should add a new trip to the trips collections", function() {
+        $vm = $controller("tripsController");
+        $vm.newTrip = {
+            name: "My test Trip"
+        };
+
+        $vm.addTrip();
+
+        expect($vm.trips.length).toBe(3);
+    })
+
+})
---
+++ src/TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,36 @@
+﻿(function () {
+	"use strict";
+
+	angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+	function tripsController(dataService) {
+		var vm = this;
+
+		vm.trips = [];
+		vm.errorMessage = "";
+		vm.isBusy = true;
+	    getTrips();
+
+		vm.newTrip = {};
+
+		vm.addTrip = function() {
+		    vm.trips.push({ name: vm.newTrip.name, created: new Date() });
+		    vm.trips = {};
+		}
+
+
+        function getTrips() {
+            dataService.getTrips()
+                .then(function(data) {
+                    vm.trips = data;
+                    vm.isBusy = false;
+                })
+                .catch(function(error) {
+                    vm.errorMessage = 'Something went wrong! ' + error;
+                    vm.isBusy = false;
+                });
+        }
+	}
+
+})();
